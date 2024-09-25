1d70f152ce58abc6dcb8604d48f81398b1789fbd
¨Lifecam98¨ <tma092024@gmail.com>
2024-09-25 20:02:42 +0200

    v2.4.1

--- script.js
+++ script.js
@@ -3,10 +3,30 @@ const themeToggleCheckbox = document.querySelector(".darkmodeswitch input");
 const toggleTheme = (isChecked) => {
   if (isChecked) {
     document.documentElement.setAttribute("theme", "dark");
+    localStorage.setItem("theme", "dark");
     console.log("Dark mode activated");
   } else {
     document.documentElement.setAttribute("theme", "light");
+    localStorage.setItem("theme", "light");
     console.log("Light mode activated");
+  }
+};
+
+const loadTheme = () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme) {
+    themeToggleCheckbox.checked = savedTheme === "dark";
+    toggleTheme(themeToggleCheckbox.checked);
+  } else if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    themeToggleCheckbox.checked = true;
+    toggleTheme(true);
+  } else {
+    themeToggleCheckbox.checked = false;
+    toggleTheme(false);
   }
 };
 
@@ -14,13 +34,4 @@ themeToggleCheckbox.addEventListener("change", () => {
   toggleTheme(themeToggleCheckbox.checked);
 });
 
-if (
-  window.matchMedia &&
-  window.matchMedia("(prefers-color-scheme: dark)").matches
-) {
-  themeToggleCheckbox.checked = true;
-  toggleTheme(true);
-} else {
-  themeToggleCheckbox.checked = false;
-  toggleTheme(false);
-}
+loadTheme();
